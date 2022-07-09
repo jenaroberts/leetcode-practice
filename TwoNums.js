8da016d182de1c138206324bf9a42345822190bd
Jena Roberts <jenaroberts73@gmail.com>
2022-07-08 21:41:40 -0400

    twosums

---
+++ TwoNums.js
@@ -0,0 +1,11 @@
+// add two index in an array to equal a target. index element being aded cannot be the same index.
+
+let twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+};
