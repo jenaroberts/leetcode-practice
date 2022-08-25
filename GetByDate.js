b833007f736096707d5c8ca4f40b5f1bbd572ec7
Jena Roberts <jenaroberts73@gmail.com>
2022-08-25 09:26:37 -0400

    filter by date

---
+++ GetByDate.js
@@ -0,0 +1,8 @@
+//on christmas eve it is time to make christmas cookies
+function timeForMilkAndCookies(date) {
+  return date.getMonth() === 11 && date.getDate() === 24;
+}
+
+const timeCookies = (date) => {
+  return date.getMonth() === 11 && date.getDate() === 24;
+};
