d9dd8aa5176003580a2306b842c633a9511148ef
Jena Roberts <jenaroberts73@gmail.com>
2022-09-08 11:15:12 -0400

    solved

---
+++ longestPrefix.js
@@ -0,0 +1,15 @@
+const longestCommonPrefix = (strs) => {
+  let result = "";
+  for (let i = 0; i < strs[0].length; i++) {
+    let set = new Set();
+    let item = "";
+    for (let j = 0; j < strs.length; j++) {
+      item = strs[j][i];
+      set.add(item);
+    }
+
+    if (set.size === 1) result += item;
+    else return result;
+  }
+  return result;
+};
